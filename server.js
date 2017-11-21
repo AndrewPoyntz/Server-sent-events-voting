9ff9b9f3a760857a270f7e9c29cb81c35ca01618
Andrew Poyntz <andrewpoyntz@hotmail.com>
2017-11-21 00:20:43 +0000

    WIP check-in of slides

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ global.totalVotes = 0;
 
 
 app.use(express.static(__dirname+'/frontend/'));
+app.use(express.static(__dirname+'/slides/'));
 app.get('/',(req,res)=>{
 	res.sendFile(__dirname+'/frontend/index.html');
 });
