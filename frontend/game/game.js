6bf58ecd6d305a90ac1d900b340317e95ed7afcd
Andrew Poyntz <andrewpoyntz@hotmail.com>
2017-11-22 00:34:09 +0000

    added noughts and crosses (tic-tac-toe) game

---
+++ frontend/game/game.js
@@ -0,0 +1,162 @@
+$(document).ready(() => {
+	let xTaken = false;
+	let oTaken = false;
+	let gameLive = false;
+	let source = new EventSource('/gameUpdates');
+	let claimXButton = $('#x');
+	let claimOButton = $('#o');
+	let youAre = $('#youAre');
+	let choice = false;
+	source.addEventListener('resetGame', (e)=> {
+		console.log('reset the game?');
+		choice = false;
+	});
+	source.addEventListener('gameState', (e)=> {
+		$('.js-wins').removeClass('active');
+		$('.x').removeClass('active');
+		$('.o').removeClass('active');
+		$('.red-line').removeClass('active');
+
+		let gameState = JSON.parse(e.data);
+		console.log(JSON.stringify(gameState));
+		xTaken = gameState.xTaken;
+		oTaken = gameState.oTaken;
+		gameLive = xTaken && oTaken;
+		if (gameLive || choice){
+			$('.choose').removeClass('active');
+		} else {
+			$('.choose').addClass('active');
+		}
+		(xTaken)? claimXButton.hide(): claimXButton.show();
+		(oTaken)? claimOButton.hide(): claimOButton.show();
+		if (choice){
+			youAre.html('You are: '+choice.toUpperCase());
+		} else if (gameLive) {
+			youAre.html('You are an observer');
+		} else {
+			youAre.html('Game is pending')
+		}
+
+		for (let square in gameState.squares){
+			let value = gameState.squares[square];
+			let box = $('.box:nth-child('+square+')');
+			let xMarker = box.children('.x');
+			let oMarker = box.children('.o');
+			if (value){
+				if (value === 'x'){
+					xMarker.addClass('active')
+				} else if (value === 'o'){
+					oMarker.addClass('active');
+				}
+			} else {
+				xMarker.removeClass('active');
+				oMarker.removeClass('active');
+			}
+		}
+		checkForWinners();
+	});
+	let claim = (value) => {
+		$.ajax({
+			url: '/claim',
+			data: {choice:value},
+			success: ()=>{}
+		});
+	};
+	claimXButton.click(function () {
+		choice = 'x';
+		claim('x');
+	});
+	claimOButton.click(function () {
+		choice ='o';
+		claim('o');
+	});
+	$('.box').click(function() {
+		if (choice && gameLive) {
+			$.ajax({
+				url: '/updateGameState',
+				data: {square:$(this).attr('id'), val:choice},
+				success: ()=>{}
+			});
+		}
+	});
+	checkForWinners = function (){
+		let xChild1 = $('.box:nth-child(1)').children('.x').hasClass('active'),
+			oChild1 = $('.box:nth-child(1)').children('.o').hasClass('active'),
+			xChild2 = $('.box:nth-child(2)').children('.x').hasClass('active'),
+			oChild2 = $('.box:nth-child(2)').children('.o').hasClass('active'),
+			xChild3 = $('.box:nth-child(3)').children('.x').hasClass('active'),
+			oChild3 = $('.box:nth-child(3)').children('.o').hasClass('active'),
+			xChild4 = $('.box:nth-child(4)').children('.x').hasClass('active'),
+			oChild4 = $('.box:nth-child(4)').children('.o').hasClass('active'),
+			xChild5 = $('.box:nth-child(5)').children('.x').hasClass('active'),
+			oChild5 = $('.box:nth-child(5)').children('.o').hasClass('active'),
+			xChild6 = $('.box:nth-child(6)').children('.x').hasClass('active'),
+			oChild6 = $('.box:nth-child(6)').children('.o').hasClass('active'),
+			xChild7 = $('.box:nth-child(7)').children('.x').hasClass('active'),
+			oChild7 = $('.box:nth-child(7)').children('.o').hasClass('active'),
+			xChild8 = $('.box:nth-child(8)').children('.x').hasClass('active'),
+			oChild8 = $('.box:nth-child(8)').children('.o').hasClass('active'),
+			xChild9 = $('.box:nth-child(9)').children('.x').hasClass('active'),
+			oChild9 = $('.box:nth-child(9)').children('.o').hasClass('active');
+
+		if( xChild1 && xChild2 && xChild3 ){
+			$('.top-hor').addClass('active');
+			$('.x-wins').addClass('active');
+		} else if(  oChild1 && oChild2 && oChild3 ){
+			$('.top-hor').addClass('active');
+			$('.o-wins').addClass('active');
+		} else if(  xChild4 && xChild5 && xChild6 ){
+			$('.mid-hor').addClass('active');
+			$('.x-wins').addClass('active');
+		} else if(  oChild4 && oChild5 && oChild6 ){
+			$('.mid-hor').addClass('active');
+			$('.o-wins').addClass('active');
+		} else if(  xChild7 && xChild8 && xChild9 ){
+			$('.bottom-hor').addClass('active');
+			$('.x-wins').addClass('active');
+		} else if(  oChild7 && oChild8 && oChild9 ){
+			$('.mid-hor').addClass('active');
+			$('.o-wins').addClass('active');
+		} else if(  xChild1 && xChild4 && xChild7 ){
+			$('.left-vert').addClass('active');
+			$('.x-wins').addClass('active');
+		} else if(  oChild1 && oChild4 && oChild7 ){
+			$('.left-vert').addClass('active');
+			$('.o-wins').addClass('active');
+		} else if(  xChild2 && xChild5 && xChild8 ){
+			$('.mid-vert').addClass('active');
+			$('.x-wins').addClass('active');
+		} else if(  oChild2 && oChild5 && oChild8 ){
+			$('.mid-vert').addClass('active');
+			$('.o-wins').addClass('active');
+		} else if(  xChild3 && xChild6 && xChild9 ){
+			$('.right-vert').addClass('active');
+			$('.x-wins').addClass('active');
+		} else if(  oChild3 && oChild6 && oChild9 ){
+			$('.right-vert').addClass('active');
+			$('.o-wins').addClass('active');
+		} else if(  xChild1 && xChild5 && xChild9 ){
+			$('.ltr-diag').addClass('active');
+			$('.x-wins').addClass('active');
+		} else if(  oChild1 && oChild5 && oChild9 ){
+			$('.ltr-diag').addClass('active');
+			$('.o-wins').addClass('active');
+		} else if(  xChild3 && xChild5 && xChild7 ){
+			$('.rtl-diag').addClass('active');
+			$('.x-wins').addClass('active');
+		} else if(  oChild3 && oChild5 && oChild7 ){
+			$('.rtl-diag').addClass('active');
+			$('.o-wins').addClass('active');
+		}
+	};
+	reset = function (){
+		$.ajax({
+			url: '/resetGame',
+			success: ()=>{}
+		});
+	};
+	$('.reset').on('click', function(){
+		reset();
+	});
+
+});
--- server.js
+++ server.js
@@ -3,37 +3,30 @@ const SSE = require('sse-node');
 const https = require('https');
 const fs = require('fs');
 const app = express();
-global.clients = {};
-global.nextID = 0;
-global.currentClients = 0;
 
+/// # ---------- voting ----------------------------
+global.voteClients = {};
+global.voteNextID = 0;
+global.voteCurrentClients = 0;
 global.voteTitle = 'Tabs or spaces?';
 global.voteData = [{name:'tabs', votes:0},{name:'spaces', votes:0}];
 global.totalVotes = 0;
 
-
-
-app.use(express.static(__dirname+'/frontend/'));
-app.use(express.static(__dirname+'/slides/'));
-app.get('/',(req,res)=>{
-	res.sendFile(__dirname+'/frontend/index.html');
-});
 app.get('/updates', (req,res)=>{
 	let client = SSE(req, res, {ping:45000});
-	global.clients[nextID] = client;
+	global.voteClients[voteNextID] = client;
 	((nextID)=>{
 		client.onClose(()=>{
-			delete global.clients[nextID];
-			global.currentClients--;
-			sendDataToAllClients(global.currentClients.toString(), 'connectedClients');
-			// console.log(global.currentClients + ' client(s) connected');
+			delete global.voteClients[voteNextID];
+			global.voteCurrentClients--;
+			sendDataToAllVoteClients(global.voteCurrentClients.toString(), 'connectedClients');
+			// console.log(global.voteCurrentClients + ' client(s) connected');
 		})
-	})(global.nextID++);
+	})(global.voteNextID++);
 	client.send(stringifyVoteData(),'init');
-	global.currentClients++;
-	sendDataToAllClients(global.currentClients.toString(), 'connectedClients');
-	//console.log(global.currentClients + ' client(s) connected');
-
+	global.voteCurrentClients++;
+	sendDataToAllVoteClients(global.voteCurrentClients.toString(), 'connectedClients');
+	//console.log(global.voteCurrentClients + ' client(s) connected');
 });
 
 app.get('/vote', (req,res)=>{
@@ -43,14 +36,14 @@ app.get('/vote', (req,res)=>{
 			global.totalVotes++;
 		}
 	}
-	sendDataToAllClients(stringifyVoteData(),'update');
+	sendDataToAllVoteClients(stringifyVoteData(),'update');
 	res.sendStatus(200);
 });
 app.get('/updateChart', (req,res)=>{
 	global.voteTitle = req.query.voteTitle;
 	global.voteData = req.query.voteData;
 	global.totalVotes = 0;
-	sendDataToAllClients(stringifyVoteData(),'init');
+	sendDataToAllVoteClients(stringifyVoteData(),'init');
 	res.sendStatus(200);
 });
 app.get('/reset', (req,res)=>{
@@ -58,18 +51,80 @@ app.get('/reset', (req,res)=>{
 		option.votes = 0;
 	}
 	global.totalVotes = 0;
-	sendDataToAllClients(stringifyVoteData(),'update');
+	sendDataToAllVoteClients(stringifyVoteData(),'update');
 	res.sendStatus(200);
 });
 
 function stringifyVoteData () {
 	return JSON.stringify({title:global.voteTitle, voteData: global.voteData, totalVotes: global.totalVotes});
 }
-function sendDataToAllClients(value, event){
-	for (clientID in global.clients){
-		global.clients[clientID].send(value, event);
+function sendDataToAllVoteClients(value, event){
+	for (let clientID in global.voteClients){
+		global.voteClients[clientID].send(value, event);
 	}
 }
+/// # ---------- voting ----------------------------
+
+
+/// # ---------- game -------------------------------
+global.gameClients = {};
+global.gameNextID = 0;
+global.xTaken = false;
+global.oTaken = false;
+global.squares = {'1':false,'2':false,'3':false,'4':false,'5':false,'6':false,'7':false,'8':false,'9':false};
+app.get('/gameUpdates', (req,res)=> {
+	let client = SSE(req, res, {ping: 45000});
+	global.gameClients[gameNextID] = client;
+	((nextID) => {
+		client.onClose(() => {
+			delete global.gameClients[gameNextID];
+		})
+	})(global.gameNextID++);
+	client.send(stringifyGameData(), 'gameState');
+});
+app.get('/resetGame', (req,res)=>{
+	global.xTaken = false;
+	global.oTaken = false;
+	global.squares = {'1':false,'2':false,'3':false,'4':false,'5':false,'6':false,'7':false,'8':false,'9':false};
+	sendDataToAllGameClients({}, 'resetGame');
+	sendDataToAllGameClients(stringifyGameData(), 'gameState');
+});
+app.get('/claim', (req,res)=>{
+	let err = false;
+	if (req.query.choice === 'x'){
+		global.xTaken = true;
+	} else if (req.query.choice === 'o'){
+		global.oTaken = true;
+	} else {
+		err= true;
+	}
+	sendDataToAllGameClients(stringifyGameData(), 'gameState');
+	res.sendStatus((!err)?200:410);
+});
+app.get('/updateGameState', (req,res)=>{
+	let err = false;
+	if (!global.squares[req.query.square]) {
+		global.squares[req.query.square] = req.query.val;
+	} else {
+		err = true;
+	}
+	sendDataToAllGameClients(stringifyGameData(), 'gameState');
+	res.sendStatus((!err)?200:423);
+});
+function stringifyGameData () {
+	return JSON.stringify({xTaken:global.xTaken, oTaken: global.oTaken, squares: global.squares});
+}
+function sendDataToAllGameClients(value, event){
+	for (let clientID in global.gameClients){
+		global.gameClients[clientID].send(value, event);
+	}
+}
+/// # ---------- game -------------------------------
+
+app.use(express.static(__dirname+'/frontend/'));
+app.get('/',(req,res)=>{
+	res.sendFile(__dirname+'/frontend/index.html');
+});
 
 const options = {
 	cert: fs.readFileSync('./server.crt'),
